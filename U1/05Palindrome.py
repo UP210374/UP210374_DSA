e15d2dfbf498df202be0f8ff2e4ae2645d95b8f4
Paulina <up210374@alumnos.upa.edu.mx>
2023-02-14 18:48:39 -0600

    update

--- U1/05Palindrome.py
+++ U1/05Palindrome.py
@@ -1,5 +1,20 @@
-word = input('Enter a word here: ')
-if word == word [::-1] :
-    print("Palindrome")
+def sentencePalindrome(s):
+	l, h = 0, len(s) - 1
+	s = s.lower()
+	while (l <= h):
+		if (not(s[l] >= 'a' and s[l] <= 'z')):
+			l += 1
+		elif (not(s[h] >= 'a' and s[h] <= 'z')):
+			h -= 1
+		elif (s[l] == s[h]):
+			l += 1
+			h -= 1
+		else:
+			return False
+	return True
+
+s = input('Enter a sentence or word: ')
+if (sentencePalindrome(s)):
+	print ("Sentence is palindrome.")
 else:
-    print("Not Palindrome")
+	print ("Sentence is not palindrome.")
